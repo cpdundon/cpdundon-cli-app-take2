cd814b57a939a1f90932b64e2952278d64ac10eb
Chris Dundon <chris.dundon@gmail.com>
2018-05-01 22:17:30 -0400

    commit of basic files

---
+++ lib/cpdundon/cli/app/take2.rb
@@ -0,0 +1,11 @@
+require "cpdundon/cli/app/take2/version"
+
+module Cpdundon
+  module Cli
+    module App
+      module Take2
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/cpdundon/cli/app/take2/version.rb
@@ -0,0 +1,9 @@
+module Cpdundon
+  module Cli
+    module App
+      module Take2
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end

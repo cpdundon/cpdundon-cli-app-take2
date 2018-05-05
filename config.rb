c6788d808f2ba26b1afbd9f92f80387da78cc278
Christopher P. Dundon <chris.dundon@gmail.com>
2018-05-05 18:38:51 -0400

    now capturing wind data

--- config.rb
+++ config.rb
@@ -1,7 +1,8 @@
-require_relative './lib/noaa_soap'
-require_relative "./lib/cli"
-require_relative './lib/get_water_level'
 require_relative './lib/gmt'
+require_relative './lib/noaa_soap'
+require_relative './lib/get_water_level'
+require_relative './lib/get_wind'
+require_relative './lib/cli'
 
 require 'time'
 require 'savon'

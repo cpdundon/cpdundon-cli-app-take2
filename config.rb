457cb7c93cc8098c5eb176ab32c48ad50ef09aab
Christopher P. Dundon <chris.dundon@gmail.com>
2018-05-05 18:20:52 -0400

    refactored the code - much cleaner.  Need to pull wind information from Robins Reef next

--- config.rb
+++ config.rb
@@ -1,3 +1,7 @@
-
+require_relative './lib/noaa_soap'
 require_relative "./lib/cli"
 require_relative './lib/get_water_level'
+require_relative './lib/gmt'
+
+require 'time'
+require 'savon'
--- lib/get_water_level.rb
+++ lib/get_water_level.rb
@@ -1,54 +1,23 @@
 require_relative '../config'
-require 'savon'
-require 'time'
 
-class GetWaterLevel
-	attr_accessor :station_id
-	attr_reader :client	
+class GetWaterLevel < NOAA_SOAP
+	
+	WSDL = "https://opendap.co-ops.nos.noaa.gov/axis/webservices/waterlevelrawsixmin/wsdl/WaterLevelRawSixMin.wsdl"
 
-	def initialize(station_id=8454000)
-		self.station_id = station_id
-		self.create_client
-	end
-
-	def create_client
-		client = Savon.client(wsdl: \
-			"https://opendap.co-ops.nos.noaa.gov/axis/webservices/waterlevelrawsixmin/wsdl/WaterLevelRawSixMin.wsdl", \
-				open_timeout: 30, \
-				read_timeout: 30, \
-				log: false, \
-				follow_redirects: true)
-
-		@client = client
-		self.client
+	def initialize
+		super WSDL
 	end
 	
-	def pull_response
-		message = {stationId: self.station_id.to_s, beginDate: gmt_less_1h, endDate: gmt_now_str, \
+	def client
+		super	
+	end
+
+	def pull_data (station_id = 8454000)
+		message = {stationId: station_id.to_s, beginDate: GMT.gmt_less_1h, endDate: GMT.gmt_now, \
 			datum: "MLLW", unit: 0, timeZone: 0}
 		
-		response = self.client.call(:get_wl_raw_six_min_and_metadata, message: message)
-		response
-	rescue Savon::SOAPFault => error
-	    fault_code = error.to_hash[:fault][:faultcode]
-	    raise CustomError, fault_code
-	end
-
-private
-
-	def gmt_now_str
-		t = Time.now.gmtime
-		t.utc.strftime "%Y%m%d %H:%M"
-	end
-
-	def gmt_less_48h
-		t = Time.now.gmtime - (2 * 24 * 60 * 60)
-		t.utc.strftime "%Y%m%d %H:%M"			
-	end
-	
-	def gmt_less_1h
-		t = Time.now.gmtime - (60 * 60)
-		t.utc.strftime "%Y%m%d %H:%M"			
+		response = self.pull_response(:get_wl_raw_six_min_and_metadata, message)
+		response.to_hash[:water_level_raw_six_min_measurements][:data][:item] #this returns a hash of the data points.
 	end
 	
 end
